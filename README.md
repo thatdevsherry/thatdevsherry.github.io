e111e626d39eda68631cebd0e274b9f58141eb3e
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-05-19 11:49:54 +0500

    Add home greeting and simple about card

--- README.md
+++
@@ -1,1 +0,0 @@
-# ShehriyarQureshi.github.io
--- polyfills.js
+++ polyfills.js
@@ -5784,8 +5784,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/sherry/Work/Github/ShehriyarQureshi.github.io/portfolio/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /home/sherry/Work/Github/ShehriyarQureshi.github.io/portfolio/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /home/sherry/Work/portfolio/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/sherry/Work/portfolio/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
